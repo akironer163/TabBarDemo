e4cd673cf67d7f3fa27f8a3fb00d1151105ce013
夏宁 <1120814158@qq.com>
2016-11-03 11:51:47 +0800

    设置tabBar选中/未选中文字的颜色 设置tabBar背景颜色

--- XYTabBarDemo/XYTabBarDemo/TabBarViewController.m
+++ XYTabBarDemo/XYTabBarDemo/TabBarViewController.m
@@ -20,6 +20,8 @@
     // Do any additional setup after loading the view.
     
     [self setupTabBarChildController];
+    
+    [self setupTabBarColor];
 }
 
 - (void)setupTabBarChildController {
@@ -30,7 +32,11 @@
         ViewController *vc = [ViewController new];
         [vc.view setBackgroundColor:[UIColor whiteColor]];
         
+        ////barButtonItem添加图片时, 取消图片的颜色渲染效果, 这样TabBar的颜色无论怎么设置, 怎么渲染, 都不会影响到图片的颜色
+        // 设置未选中图片(取消渲染,原始效果)
         vc.tabBarItem.image = [[UIImage imageNamed:imageArray[2]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        
+        // 设置选中图片(取消渲染,原始效果)
         vc.tabBarItem.selectedImage = [[UIImage imageNamed:[imageArray[2] stringByAppendingString:@"_selected"]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
         vc.title = titleArray[i];
         
@@ -40,19 +46,16 @@
     }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupTabBarColor {
+    
+    // 背景图颜色
+    [self.tabBar setBarTintColor:[UIColor purpleColor]];
+    
+    // 未选中状态的标题颜色
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14], NSForegroundColorAttributeName:[UIColor greenColor]} forState:UIControlStateNormal];
+    
+    // 选中状态的标题颜色
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14], NSForegroundColorAttributeName:[UIColor purpleColor]} forState:UIControlStateSelected];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
